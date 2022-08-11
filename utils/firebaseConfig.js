26f96b2b6e28832531bed76ed6f58951fa92288d
Бек Сламбек <slambek_b0607@gmail.com>
2022-08-11 23:48:35 +0600

    hide secret data
    Co-authored-by: Dastan Ozgeldi <Dositan@users.noreply.github.com>

--- utils/firebaseConfig.js
+++ utils/firebaseConfig.js
@@ -1,18 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSdZH0i8y_ggOkmKEsa2mPBdcPPRu7bNA",
-  authDomain: "todo-3c64f.firebaseapp.com",
-  projectId: "todo-3c64f",
-  storageBucket: "todo-3c64f.appspot.com",
-  messagingSenderId: "270571642912",
-  appId: "1:270571642912:web:409035b85a0d3a816a1c45",
-  measurementId: "G-WDWV7HYH8E"
+  apiKey: process.env.API_KEY,
+  authDomain: procces.env.AUTH_DOMAIN,
+  projectId: procces.env.PROJECT_ID,
+  storageBucket: procces.env.STORAGE_BUCKET,
+  messagingSenderId: procces.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const database = getFirestore(app)
+export const database = getFirestore(app);
+export const auth = getAuth(app);
